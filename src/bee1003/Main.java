1a6b04b448008fd96e4b947d4f5d37e2fb4220d8
Bruno Prata de Freitas <brunprata@gmail.com>
2026-07-20 15:55:08 -0300

    Adiciona solução do exercício 1003 - Soma Simples

---
+++ src/bee1003/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ 
+/*
+ * Beecrowd 1003 - Soma Simples
+ * Linguagem: Java
+ * Data: 20/07/2026
+ * Status: Accepted
+ */
+public class Main {
+ 
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner (System.in);
+        int A, B;
+        
+        A = teclado.nextInt();
+        B = teclado.nextInt();
+        
+        System.out.println ("SOMA = " + (A + B));
+ 
+    }
+ 
+}
