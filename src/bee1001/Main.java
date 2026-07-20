745f4184ff5ff7520a5f879dfd747c64ea84efa6
Bruno Prata de Freitas <brunprata@gmail.com>
2026-07-20 14:38:00 -0300

    Adiciona solução do exercício 1001 - Extremamente Básico

---
+++ src/bee1001/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/*
+ * Beecrowd 1001 - Extremamente Básico
+ * Linguagem: Java
+ * Data: 20/07/2026
+ * Status: Accepted
+ */
+
+public class Main {
+ 
+    public static void main (String[] args){
+ 
+    Scanner teclado = new Scanner (System.in);
+
+    int A, B;
+
+    A = teclado.nextInt();
+    B = teclado.nextInt();
+
+    System.out.println ("X = " + (A + B));
+
+
+    }
+ 
+}
