19aeb0da641dd3d75098d74fe50620f0028580f7
Bruno Prata de Freitas <brunprata@gmail.com>
2026-07-20 14:19:25 -0300

    Adiciona solução do exercício 1000 - Hello World

---
+++ src/bee1000/Main.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+ 
+/*
+ * Beecrowd 1001 - Extremamente Básico
+ * Linguagem: Java
+ * Status: Accepted
+ */
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+     System.out.println("Hello World!");
+    }
+ 
+}
