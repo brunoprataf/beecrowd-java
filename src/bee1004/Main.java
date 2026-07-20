3d0b27cd278f238b4594b4898a4288f0e35d47b9
Bruno Prata de Freitas <brunprata@gmail.com>
2026-07-20 16:07:54 -0300

    Adiciona solução do exercício 1004 - Produto Simples

--- src/bee1004/Main.java
+++ src/bee1004/Main.java
@@ -1,1 +1,24 @@
-
+import java.util.Scanner;
+ 
+/*
+ * Beecrowd 1004 - Produto Simples
+ * Linguagem: Java
+ * Data: 20/07/2026
+ * Status: Accepted
+ */
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+ Scanner teclado = new Scanner (System.in);
+ int A, B;
+ 
+ A = teclado.nextInt();
+ B = teclado.nextInt();
+ 
+ System.out.println ("PROD = " + (A * B));
+ 
+    }
+ 
+}
