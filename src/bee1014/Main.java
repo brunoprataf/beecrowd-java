94c239fc9d07f5b7ad4ca9ea7ce95762ce8624fb
Bruno Prata de Freitas <brunprata@gmail.com>
2026-07-22 10:33:45 -0300

    Adiciona solução do exercício 1014 - Consumo

--- src/bee1014/Main.java
+++ src/bee1014/Main.java
@@ -1,1 +1,28 @@
-
+import java.util.Scanner;
+ 
+/*
+ * Beecrowd 1014 - Consumo
+ * Linguagem: Java
+ * Data: 22/07/2026
+ * Status: Accepted
+ */  
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        int x;
+        double y, resultado;
+        
+        x = teclado.nextInt();
+        y = teclado.nextDouble();
+        resultado = x / y;
+        
+        
+        System.out.printf("%.3f km/l%n", resultado);
+ 
+    
+    }
+ 
+}
