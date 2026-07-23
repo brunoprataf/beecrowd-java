4bae9bf65b829b4e2f109184428ffda13f67e128
Bruno Prata de Freitas <brunprata@gmail.com>
2026-07-23 17:41:23 -0300

    Adiciona solução do exercício 1035 - Teste de Seleção 1

---
+++ src/bee1035/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+ 
+/*
+ * Beecrowd 1035 - Teste de Seleção 1
+ * Linguagem: Java
+ * Data: 23/07/2026
+ * Status: Accepted
+ */ 
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        
+  Scanner teclado = new Scanner(System.in);
+    int A, B, C, D;
+
+    A = teclado.nextInt();
+    B = teclado.nextInt();
+    C = teclado.nextInt();
+    D = teclado.nextInt();
+
+    if (B > C &&
+            D > A &&
+            (C + D) > (A + B) &&
+            C > 0 &&
+            D > 0 &&
+            A % 2 == 0) {
+
+        System.out.println("Valores aceitos");
+
+    } else {
+
+        System.out.println("Valores nao aceitos");
+
+    }
+    
+    }
+ 
+}
