767ba03aa20469b6a64fda9e188149e2aac9ee1c
Bruno Prata de Freitas <brunprata@gmail.com>
2026-07-28 10:41:46 -0300

    Adiciona solução do exercício 1042 - Sort Simples

---
+++ src/bee1042/Main.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+ 
+/*
+ * Beecrowd 1042 - Sort Simples
+ * Linguagem: Java
+ * Data: 27/07/2026
+ * Status: Accepted
+ */
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+       Scanner teclado = new Scanner(System.in);
+
+    int A, B, C;
+
+    A = teclado.nextInt();
+    B = teclado.nextInt();
+    C = teclado.nextInt();
+
+    if (A <= B && B <= C) {
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C + "\n");
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+    } else if (B <= A && A <= C) {
+        System.out.println(B);
+        System.out.println(A);
+        System.out.println(C + "\n");
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+    } else if (C <= A && A <= B) {
+        System.out.println(C);
+        System.out.println(A);
+        System.out.println(B + "\n");
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+    } else if (B <= C && C <= A) {
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(A + "\n");
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+    } else if (C <= B && B <= A) {
+        System.out.println(C);
+        System.out.println(B);
+        System.out.println(A + "\n");
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+    }else if (A <= C && C <= B) {
+        System.out.println(A);
+        System.out.println(C);
+        System.out.println(B + "\n");
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+    }
+ 
+    }
+ 
+}
