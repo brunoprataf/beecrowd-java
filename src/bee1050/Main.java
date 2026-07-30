0fff37986605f76c0b5aa7d68efd5072730a85c5
Bruno Prata de Freitas <brunprata@gmail.com>
2026-07-30 10:25:55 -0300

    Adiciona solução do exercício 1050 - DDD

---
+++ src/bee1050/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+ 
+/*
+ * Beecrowd 1050 - DDD
+ * Linguagem: Java
+ * Data: 30/07/2026
+ * Status: Accepted
+ */
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+    Scanner teclado = new Scanner(System.in);
+    int A;
+    A = teclado.nextInt();
+
+    if (A == 61) {
+        System.out.println("Brasilia");
+    } else if (A == 71) {
+        System.out.println("Salvador");
+    }else if (A == 11) {
+        System.out.println("Sao Paulo");
+    }else if (A == 21) {
+        System.out.println("Rio de Janeiro");
+    }else if (A == 32) {
+        System.out.println("Juiz de Fora");
+    }else if (A == 19) {
+        System.out.println("Campinas");
+    }else if (A == 27) {
+        System.out.println("Vitoria");
+    }else if (A == 31) {
+        System.out.println("Belo Horizonte");
+    }else {
+        System.out.println("DDD nao cadastrado");
+    }
+ 
+    }
+ 
+}
