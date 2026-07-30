047f244e0d8ad18d8f1121960fa8b41ff705c7a4
Bruno Prata de Freitas <brunprata@gmail.com>
2026-07-30 17:28:10 -0300

    Adiciona solução do exercício 1059 - Números Pares

---
+++ src/bee1059/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+ 
+/*
+ * Beecrowd 1059 - Números Pares
+ * Linguagem: Java
+ * Data: 30/07/2026
+ * Status: Accepted
+ */
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+        for (int i = 2; i <= 100; i += 2) {
+        System.out.println(i);
+    }
+ 
+    }
+ 
+}
