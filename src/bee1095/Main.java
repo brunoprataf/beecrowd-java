f71884c543b702ea44e7953c1af490bd317ec77a
Bruno Prata de Freitas <brunprata@gmail.com>
2026-08-11 09:26:50 -0300

    Adiciona solução do exercício 1095 - Sequencia IJ 1

---
+++ src/bee1095/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ 
+/*
+ * Beecrowd 1095 - Sequencia IJ 1
+ * Linguagem: Java
+ * Data: 11/08/2026
+ * Status: Accepted
+ */
+ 
+public class Main {
+ 
+    public static void main(String[] args){
+ 
+        for (int I = 1, J = 60; J >= 0; I += 3, J -= 5) {
+
+        System.out.println("I=" + I + " J=" + J);
+    }
+ 
+    }
+ 
+}
