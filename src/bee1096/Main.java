b58fe70475fe79cfddbc2f3969e07d808a6572a1
Bruno Prata de Freitas <brunprata@gmail.com>
2026-08-11 09:40:36 -0300

    Adiciona solução do exercício 1096 - Sequencia IJ 2

---
+++ src/bee1096/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ 
+/*
+ * Beecrowd 1096 - Sequencia IJ 2
+ * Linguagem: Java
+ * Data: 11/08/2026
+ * Status: Accepted
+ */
+ 
+public class Main {
+ 
+    public static void main(String[] args){
+ 
+        
+    for (int I = 1; I <= 9; I += 2) {
+
+        for (int J = 7; J >= 5; J--) {
+
+            System.out.println("I=" + I + " J=" + J);
+        }
+    }
+ 
+    }
+ 
+}
