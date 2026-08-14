03cd10abd16cdd40b43b68543957b78c093d106c
Bruno Prata de Freitas <brunprata@gmail.com>
2026-08-14 15:17:08 -0300

    Adiciona solução do exercício 1149 - Somando Inteiros Consecutivos

---
+++ src/bee1149/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+ 
+/*
+ * Beecrowd 1149 - Somando Inteiros Consecutivos
+ * Linguagem: Java
+ * Data: 13/08/2026
+ * Status: Accepted
+ */
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+    Scanner teclado = new Scanner(System.in);
+
+    int A = teclado.nextInt();
+    int N = teclado.nextInt();
+
+    while (N <= 0) {
+        N = teclado.nextInt();
+    }
+
+    int soma = 0;
+
+    for (int i = 0; i < N; i++) {
+        soma += A + i;
+    }
+
+  System.out.println(soma);  
+ 
+    }
+ 
+}
