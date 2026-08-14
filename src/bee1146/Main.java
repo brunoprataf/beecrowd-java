9e6e3eff43b56b07d59ef14802c8950b363e94e5
Bruno Prata de Freitas <brunprata@gmail.com>
2026-08-14 14:42:47 -0300

    Adiciona solução do exercício 1146 - Sequências Crescentes

---
+++ src/bee1146/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+ 
+/*
+ * Beecrowd 1146 - Sequências Crescentes
+ * Linguagem: Java
+ * Data: 13/08/2026
+ * Status: Accepted
+ */
+ 
+public class Main {
+ 
+    public static void main(String[] args){
+ 
+    Scanner teclado = new Scanner(System.in);
+
+int X = teclado.nextInt();
+
+while (X != 0) {
+
+    StringBuilder linha = new StringBuilder();
+
+    for (int i = 1; i <= X; i++) {
+
+        if (i == X) {
+            linha.append(i);
+        } else {
+            linha.append(i).append(" ");
+        }
+    }
+
+    System.out.println(linha);
+
+    X = teclado.nextInt();
+}
+ 
+    }
+ 
+}
